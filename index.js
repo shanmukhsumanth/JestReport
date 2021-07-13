6c1e02f4e84f9d24899d9868e7b97552c45cdb05
mangeshpa <86788640+mangeshpa@users.noreply.github.com>
2021-07-13 19:38:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// importing express framework
-const express = require("express");
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-const app = express();
-
-// Respond with "hello world" for requests that hit our root "/"
-app.get("/", function (req, res) {
- return res.send("Hello World");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node!\n'
+  res.end(msg);  
 });
 
-// listen to port 7000 by default
-app.listen(process.env.PORT || 7000, () => {
-  console.log("Server is running");
-  done();
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);    
+})
+afterAll(async done => {
+   globalRedisClient.unref();
+   done();
 });
-
-module.exports = app;
-
+afterAll(async () => {
+  await app.delete()
+})
