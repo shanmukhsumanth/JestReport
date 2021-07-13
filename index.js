976ce7e7959e51dd004878073cb21fb47f2373eb
mangeshpa <86788640+mangeshpa@users.noreply.github.com>
2021-07-13 19:49:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
-  await app.delete()
-  done();
+  listen.end();
 })
