e32781cb159d665c9a32bc0364d0d428d7e310fc
mangeshpa <86788640+mangeshpa@users.noreply.github.com>
2021-07-14 14:34:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const http = require('http');
+/* Events
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+ - Similar to how the DOM works, Node.js triggers events and handles the callback functions
+ - Many objects in Node emit events, in general, these are inherited from the EventEmitter constructor
+ */
+// Loading the EventEmitter constructor
+var EventEmitter = require('events').EventEmitter;
+
+//In this case, we want the logger to emit Events by adding a listener
+var logger = new EventEmitter();
+
+// The following code demonstrates the syntax for listening to the error event,
+// and executing the callback function
+logger.on('error', function(message) {
+    console.log('ERR: ' + message);
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+// The following code triggers the 'error' event
+logger.emit('error', 'This is the first error');
+logger.emit('error', 'This is the second error');
 
-console.log("Server running at http://localhost:%d", port);
-done();
+/* Run the following cmd to start node and listen/generate events
+ node '02 - Events.js'
+
+ You should see the following output:
+
+ ERR: This is the first error
+ ERR: This is the second error
+
+ */
