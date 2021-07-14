11a4ea26f4479cacb2acf6335cd27d64bd1bec9b
mangeshpa <86788640+mangeshpa@users.noreply.github.com>
2021-07-14 15:15:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const async = require('async');
-async.map(
-    [1,2,3],
-    function(val, callback){
-        // loop callbacks
-        setTimeout(function(){
-            callback(null, val * val);
-        }, 1000);
-    }, function(err, Tarr){
-        // final callback
-    }
-)
+async.series([
+    function(callback){
+        http.get('url-1', function(res1){
+            callback(null, res.data);
+        })
+    },
+    function(callback){
+        http.get('url-2', function(res2){
+            callback(null, res.data);
+        })
+    },
+    ...
+], function(err, data){
+    //data => [res1.data, res2.data, ...]
+})
