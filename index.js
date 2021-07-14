21cab00d6b1eaeeafe25e48bf43f16af8cfe20b7
mangeshpa <86788640+mangeshpa@users.noreply.github.com>
2021-07-14 14:22:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  static res.statusCode = 200;
-  static const msg = 'Hello Node!\n'
+   res.statusCode = 200;
+   const msg = 'Hello Node!\n'
   res.end(msg);  
 });
-
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);  
 })
+done();
