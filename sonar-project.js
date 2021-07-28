8ed0ef4e23548b8001f68eb5574d937404cba51d
shanmukhsumanth <shanmukhsumanth111@gmail.com>
2021-07-28 11:53:38 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,7 +4,10 @@ sonarqubeScanner({
   serverUrl: 'http://18.223.255.187:9000/',
        options : {
        'sonar.sources': '.',
+        'sonar.tests' : '.' ,
        'sonar.inclusions' : '**',
+       'sonar.test.inclusions': './*.test.js','./*.test.jsx'
+
 //        'sonar.coverageReportPaths' : './Junit.xml'
        },
 }, () => {});
